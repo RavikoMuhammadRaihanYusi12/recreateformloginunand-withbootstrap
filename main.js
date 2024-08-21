b1a05122ece3a780b414c9554b4157c16a9ebc85
ravikomuhammad <101442920+RavikoMuhammadRaihanYusi12@users.noreply.github.com>
2024-08-21 19:32:56 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+let nama = prompt('Selamat datang di Portal UNAND BETA VERSION , Silahkan isi nama lengkap anda dibawah ini :')
+alert('Selamat datang ' + nama)
+alert('Silahkan mengisi Form tersebut.')
